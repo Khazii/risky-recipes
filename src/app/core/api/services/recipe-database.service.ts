91f0ef7fe5bb3d650431605c43171c1f80d6009b
Ollie <oliverbrennan05@gmail.com>
2021-03-21 22:01:08 +0000

    Firebase / proxy fixes

--- src/app/core/api/services/recipe-database.service.ts
+++ src/app/core/api/services/recipe-database.service.ts
@@ -1,21 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Store } from '@ngxs/store';
+import { ClearRecipeResults } from 'src/app/states/recipes/actions/clear-recipes';
 import { SetRecipeList } from 'src/app/states/recipes/actions/set-recipes';
 import { RecipeSearchQuery } from '../models/recipe-search.model';
 import { ModelRecipeList } from '../models/recipe.model';
 
 @Injectable({ providedIn: 'root' })
 export class RecipeDatabaseService {
+  recipeUrl = `http://www.recipepuppy.com/api`;
+
   constructor(private _httpClient: HttpClient, private _store: Store) {}
 
   getRecipes(searchQuery: RecipeSearchQuery) {
     this._httpClient
       .get<ModelRecipeList>(
-        `http://www.recipepuppy.com/api/?${searchQuery.ingredients}${searchQuery.search}&p=1`
+        `${this.recipeUrl}/?${searchQuery.ingredients}${searchQuery.search}&p=1`
       )
-      .subscribe((res) => {
-        this._store.dispatch(new SetRecipeList(res.results, searchQuery));
-      });
+      .subscribe(
+        (res) => {
+          this._store.dispatch(new SetRecipeList(res.results, searchQuery));
+        },
+        () => this._store.dispatch(new ClearRecipeResults())
+      );
   }
 }
